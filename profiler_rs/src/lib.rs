cc9626eb345653e86d624f537a581cb2b8ce680b
Paul <xxx.xx>
2023-10-25 18:42:35 +0800

    feat: opti structure

---
+++ profiler_rs/src/lib.rs
@@ -0,0 +1,19 @@
+#[allow(unused_imports)]
+use profiler_macro::time_profiler;
+
+#[test]
+fn test() {
+    #[time_profiler()] // default: with the function's name
+    fn outer(t: u64) {
+        let res = inner(t - 1);
+        println!("outer:{t}, from inner: {res}");
+    }
+
+    #[time_profiler("inner")]
+    fn inner(t: u64) -> u64 {
+        println!("inner:{t}");
+        1
+    }
+
+    outer(4);
+}
