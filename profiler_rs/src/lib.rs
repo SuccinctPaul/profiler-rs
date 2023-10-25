0b9628c72820059f5c99e3448f492ec88cabcf61
Paul Cheng <chengyuejia@foxmail.com>
2023-10-25 23:56:07 +0800

    add another test

--- profiler_rs/src/lib.rs
+++ profiler_rs/src/lib.rs
@@ -22,3 +22,45 @@ fn test() {
 
     outer(4);
 }
+
+#[test]
+fn test_trait() {
+    trait Animal {
+        // For now, time_profiler can't be used for trait.
+        fn name() -> String;
+    }
+
+    struct Dog;
+    impl Dog {
+        #[time_profiler()] // default: with the function's name
+        pub fn shout() {
+            println!("wowowoow");
+            println!("I'm a {:?}", Self::name());
+        }
+    }
+    impl Animal for Dog {
+        #[time_profiler()] // default: with the function's name
+        fn name() -> String {
+            String::from("dog")
+        }
+    }
+
+    struct Cat;
+    impl Cat {
+        #[time_profiler("talk")]
+        pub fn talk() {
+            println!("mimimi");
+            println!("I'm a {:?}", Self::name());
+        }
+    }
+    impl Animal for Cat {
+        #[time_profiler("cat_name")] // default: with the function's name
+        fn name() -> String {
+            String::from("cat")
+        }
+    }
+
+    Dog::shout();
+    println!();
+    Cat::talk();
+}
